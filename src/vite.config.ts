ddda9c4f3402d5dcd52015c42a9b8fb857a8deb7
VFlora72 <viveka.flora@bsu.edu>
2025-11-20 11:25:07 -0500

    Change base path in Vite config
    Updated base path to match new GitHub repo name.

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pbl2cardinalbooksweb/', // Updated to match your GitHub repo name
+  base: '/Cardinalbooks/', // Updated to match your GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
